9ac15f2b9a0d65d526a4055ece1fc920f83c5db7
deji <dejiwilliams9@gmail.com>
2025-02-06 00:11:34 +0100

    last commit.

--- src/components/ui/CustomTable/customTable.tsx
+++ src/components/ui/CustomTable/customTable.tsx
@@ -25,61 +25,65 @@ const Table = <T,>({
   });
 
   return (
-    <div className="flex flex-col justify-between w-full overflow-x-auto">
-      <table className="w-full table-auto border-collapse">
-        <thead className="bg-purpleFragments-#BEADFF text-[14px] font-semibold leading-[16.94px] text-left">
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  className="py-[7.5px] px-[10px] text-left align-middle whitespace-nowrap"
-                >
-                  {flexRender(header.column.columnDef.header, header.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody className="text-[14px] text-[#181818] font-normal leading-[16.94px]">
-          {error ? (
-            <tr>
-              <td colSpan={columns.length} className="text-center text-red-500 py-4">
-                An error occurred
-              </td>
-            </tr>
-          ) : isLoading ? (
-            <tr>
-              <td colSpan={columns.length} className="text-center py-4">
-                Loading....
-              </td>
-            </tr>
-          ) : (
-            table.getRowModel()?.rows?.map((row, index) => (
-              <tr
-                key={row.id}
-                className={`${index % 2 === 0 ? "bg-[#FFFFFF]" : "bg-[#E4E4E4]"} hover:bg-gray-100`}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <td
-                    key={cell.id}
-                    className="px-[10px] py-[10px] text-ellipsis overflow-hidden whitespace-nowrap"
+    <div className="flex flex-col justify-between w-full overflow-hidden">
+      <div className="overflow-x-auto">
+        <table className="w-full table-auto border-collapse">
+          <thead className="bg-purpleFragments-#BEADFF text-[14px] font-semibold leading-[16.94px] text-left">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    className="py-[7.5px] px-[10px] text-left align-middle whitespace-nowrap"
                   >
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </td>
+                    {flexRender(header.column.columnDef.header, header.getContext())}
+                  </th>
                 ))}
               </tr>
-            ))
-          )}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody className="text-[14px] text-[#181818] font-normal leading-[16.94px]">
+            {error ? (
+              <tr>
+                <td colSpan={columns.length} className="text-center text-red-500 py-4">
+                  An error occurred
+                </td>
+              </tr>
+            ) : isLoading ? (
+              <tr>
+                <td colSpan={columns.length} className="text-center py-4">
+                  Loading....
+                </td>
+              </tr>
+            ) : (
+              table.getRowModel()?.rows?.map((row, index) => (
+                <tr
+                  key={row.id}
+                  className={`${index % 2 === 0 ? "bg-[#FFFFFF]" : "bg-[#E4E4E4]"} hover:bg-gray-100`}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <td
+                      key={cell.id}
+                      className="px-[10px] py-[10px] text-ellipsis overflow-hidden whitespace-nowrap"
+                    >
+                      {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                    </td>
+                  ))}
+                </tr>
+              ))
+            )}
+          </tbody>
+        </table>
+      </div>
 
       {currentPage && onPageChange && totalPages && (
-        <CustomPagination
-          currentPage={currentPage}
-          onPageChange={onPageChange}
-          totalPages={totalPages}
-        />
+        <div className="overflow-hidden">
+          <CustomPagination
+            currentPage={currentPage}
+            onPageChange={onPageChange}
+            totalPages={totalPages}
+          />
+        </div>
       )}
     </div>
   );
